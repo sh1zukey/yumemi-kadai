e02a4bb7e610148cc2186299b84d3cf087cca01a
takashun <s1832087py@s.chibakoudai.jp>
2021-06-04 00:06:32 +0900

    総人口の取得処理実装

--- store/index.js
+++ store/index.js
@@ -1,7 +1,17 @@
 export const state = () => ({
   prefData: [],
   selectedPrefCodes: [],
+  prefPopulationData: {},
 })
+
+export const getters = {
+  selectedPrefCodes: (state) => {
+    return state.selectedPrefCodes
+  },
+  prefPopulationData: (state) => {
+    return state.prefPopulationData
+  },
+}
 
 export const mutations = {
   setPrefs(state, prefData) {
@@ -10,6 +20,9 @@ export const mutations = {
   setSelectedPrefCodes(state, selectedPrefCodes) {
     state.selectedPrefCodes = selectedPrefCodes
   },
+  setPrefPopulationData(state, prefPopulationData) {
+    state.prefPopulationData = prefPopulationData
+  },
 }
 
 export const actions = {
@@ -17,10 +30,30 @@ export const actions = {
     const response = await this.$axios
       .get('/api/prefectures')
       .then((response) => response)
-      .catch((err) => err.response)
+      .catch((error) => error.response)
 
     if (response.status === 200) {
       commit('setPrefs', response.data)
     }
+  },
+  async updatePrefPopulationData({ state, commit }) {
+    const prefPopulationData = state.prefPopulationData
+    const promises = []
+
+    state.selectedPrefCodes.forEach((prefCode) => {
+      if (!prefPopulationData[String(prefCode)]) {
+        promises.push(this.$axios.get('/api/population/' + prefCode))
+      }
+    })
+
+    const responses = await Promise.all(promises)
+      .then((responses) => responses)
+      .catch((error) => error.response)
+
+    responses.forEach((response) => {
+      const prefCode = response.config.url.split('/').slice(-1)[0]
+      prefPopulationData[String(prefCode)] = response.data
+      commit('setPrefPopulationData', prefPopulationData)
+    })
   },
 }
